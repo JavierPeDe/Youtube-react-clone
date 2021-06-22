3059bfad19c4f17ba77621a0f3b2c009a364c66e
JavierPerez <javip107@gmail.com>
2021-06-22 10:56:23 +0200

    feat: appComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import youtube from './apis/youtube';
 import './App.scss';
 
- export const App=()=> {
+export const App = () => {
+  const [newVideos, setNewVideos] = useState({
+    videos: [],
+    seledvideos: null
+  })
+  const handleSubmit = async (textFromSearchBar) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: textFromSearchBar,
+      }
+    }
+    )
+    setNewVideos((preNewVideos) => ({
+      ...preNewVideos,
+      videos: response.data.items
+    }))
+  }
+
+
   return (
     <div className="App">
-     <h1>Clone Youtube</h1>
+      <button onClick={() => handleSubmit('game')} >Play</button>
+      <h1>Clone Youtube</h1>
     </div>
   );
 }
---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const KEY = 'AIzaSyBE1tBjBvamyQodMiYchOCEvVzfI5dcPCw'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 6,
+        key: KEY
+    }
+
+})
