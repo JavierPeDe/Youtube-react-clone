a13fdc88a4b265ec22f7e2986b6d9a1035693d70
javierPerez <javip107@gmail.com>
2021-06-22 17:13:45 +0200

    Search Bar component style

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './index.scss';
+export const SearchBar = ({ handleFormSubmit }) => {
+  const [textFromSearchBar, setTextFromSearchBar] = useState('');
 
-export const SearchBar = ({handleFormSubmit}) => {
-    const [textFromSearchBar, setTextFromSearchBar] = useState('')
-
-    const handleOnChange = evt => {
-        setTextFromSearchBar(evt.target.value)
-        console.log(textFromSearchBar)
-    }
-    const handleSubmit = evt => {
-        evt.preventDefault();
-        handleFormSubmit(textFromSearchBar);
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <div >
-                <i type='submit' className='material-icons'>search</i>
-                <input type='text' onChange={handleOnChange} placeholder='Search videos' />
-            </div>
-        </form>
-    )
-}
+  const handleOnChange = (evt) => {
+    setTextFromSearchBar(evt.target.value);
+    console.log(textFromSearchBar);
+  };
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleFormSubmit(textFromSearchBar);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="container-form">
+        <input
+          type="text"
+          onChange={handleOnChange}
+          placeholder="Search videos"
+        />
+        <i
+          type="button"
+          onClick={handleSubmit}
+          className="material-icons style-icon"
+        >
+          search
+        </i>
+      </div>
+    </form>
+  );
+};
